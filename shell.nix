79cb948b8d7b9a438629d1e94a523d17e517972d
José Romildo Malaquias <malaquias@gmail.com>
2019-05-20 09:07:38 -0300

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ nixpkgs ? import <nixpkgs> {} } :
+
+let
+  inherit (nixpkgs) pkgs;
+  pythonPackages = pkgs.python3Packages;
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "my-django-env-0";
+  buildInputs = [
+    pythonPackages.python
+    pythonPackages.django_2_1
+    pkgs.jetbrains.pycharm-professional
+    pkgs.emacs
+    pkgs.vscode
+  ];
+}
